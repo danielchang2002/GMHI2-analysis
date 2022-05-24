31913f750dde7d73f8bb0d1d81cb9a6da4f3cf92
danielchang2002 <danielchang2002@gmail.com>
2022-05-24 16:13:38 -0500

    :book: init

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def get_species():
+  return pd.read_csv("../data/X_species.csv", index_col=[0, 1])
+
+def get_labels():
+  return pd.read_csv("../data/y.csv", index_col=[0, 1])
+
+def get_labels_all():
+  return pd.read_csv("../data/y_all.csv", index_col=[0, 1])

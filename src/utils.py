feef5b88f923b53e19626aa5731fc6bc2472ea91
danielchang2002 <danielchang2002@gmail.com>
2022-07-08 18:30:08 -0500

    :fire: idk

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,12 @@ import numpy as np
 def get_species():
   return pd.read_csv("../data/X_species.csv", index_col=[0, 1])
 
+def get_taxonomy():
+  return pd.read_csv("../data/X_all.csv", index_col=[0, 1])
+
+def get_counts():
+  return pd.read_csv("../data/counts.csv", index_col=[0, 1])
+
 def get_labels():
   return pd.read_csv("../data/y.csv", index_col=[0, 1])
 
